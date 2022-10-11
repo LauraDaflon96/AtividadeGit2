a07f491fc0139c8df39254bdbdbda065030f97f6
Laura Daflon <lauradaflon@poli.ufrj.br>
2022-10-11 15:14:55 -0300

    Initial Commit

---
+++ AtividadeGit2/AtividadeGit2App.swift
@@ -0,0 +1,17 @@
+//
+//  AtividadeGit2App.swift
+//  AtividadeGit2
+//
+//  Created by Laura Daflon dos Santos Santana Meira on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AtividadeGit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
